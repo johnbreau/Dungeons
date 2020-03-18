27cf52dc540c9a79b6287c4285e7612f9d571dae
John Breau <john.breau@vml.com>
2020-03-18 11:36:01 -0500

    restructured character sheet for MD

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,44 @@ import { DDService } from './services/dd.service';
 import { PokeService } from './services/pokeService.service';
 import { AbilityScoreAdjustmentService } from './services/abilityScoreAdjustment.service';
 import { PokemonComponent } from './components/pokemon/pokemon.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LayoutModule } from '@angular/cdk/layout';
+
+//
+// Form Controls
+//
+
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
+import { MatNativeDateModule, MatCardModule } from '@angular/material';
+// import { MatMomentDateModule } from '@angular/material-moment-adapter';
+
+const modules: any[] = [
+
+  LayoutModule,
+  MatCardModule,
+  MatAutocompleteModule,
+  MatCheckboxModule,
+  MatDatepickerModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatRadioModule,
+  MatSelectModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+
+  MatNativeDateModule,
+  // MatMomentDateModule,
+
+];
 
 @NgModule({
   declarations: [
@@ -23,7 +61,12 @@ import { PokemonComponent } from './components/pokemon/pokemon.component';
     ReactiveFormsModule,
     FormsModule,
     HttpClientModule,
+    BrowserAnimationsModule,
+    ...modules,
   ],
+    exports: [
+      ...modules
+    ],
   providers: [DiceService, AbilityScoreAdjustmentService, PokeService, DDService],
   bootstrap: [AppComponent]
 })
